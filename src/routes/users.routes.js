b095f08f094957b2295d4e943c35ecc8436ae8c5
DamianURU <earcollar@gmail.com>
2021-03-28 18:10:48 -0400

    Commit 2

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,7 +3,12 @@ const routes = Router();
 const Users = require("../models/users");
 const bcrypt = require("bcryptjs");
 const { createToken, verifyToken } = require("../config/auth");
+const { verify } = require("jsonwebtoken");
 require("dotenv").config();
+const client = require("twilio")(
+  process.env.ACCOUNT_SID,
+  process.env.AUTH_TOKEN
+);
 
 routes.post("/api/login", async (req, res) => {
   const user = await Users.getByEmail(req.body.email);
@@ -27,10 +32,31 @@ routes.post("/api/create", async (req, res) => {
   if (verificationEmail !== undefined)
     return res.status(500).send({ error: "Email already in use" });
   req.body.password = bcrypt.hashSync(req.body.password, 10);
-  const result = await Users.insert(req.body);
+  client.verify
+    .services(process.env.SERVICE_SID)
+    .verifications.create({
+      to: `+${req.body.phone}`,
+      channel: "sms",
+    })
+    .then((data) => {
+      res.sendStatus(200).send(data);
+    });
+  if (await verify(req, res)) const result = await Users.insert(req.body);
   if (result != null) return res.sendStatus(200);
   res.sendStatus(500);
 });
+
+async function verify(req, res) {
+  client.verify
+    .services(process.env.SERVICE_SID)
+    .verificationChecks.create({
+      to: `+${req.body.phone}`,
+      code: req.body.code,
+    })
+    .then((data) => {
+      res.status(200).send(data);
+    });
+}
 
 routes.post("/api", verifyToken, async (req, res) => {
   console.log(req.token.data.id);

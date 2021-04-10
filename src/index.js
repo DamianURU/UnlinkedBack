9d2a9a088376478ca973344e427edbd1a4900c83
DamianURU <earcollar@gmail.com>
2021-04-10 11:10:56 -0400

    port change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("./server");
 require("dotenv").config();
 
-app.listen(process.env.PORT, function () {
+app.listen(app.get(process.env.PORT), function () {
   console.log("App listening on port " + process.env.PORT);
 });

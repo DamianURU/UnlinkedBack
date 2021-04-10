ca8446553d5f1047f1b5c43a3e8d04a751231bf6
DamianURU <earcollar@gmail.com>
2021-04-10 11:18:08 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require("./server");
 require("dotenv").config();
 
-app.listen(app.get(process.env.PORT), function () {
+app.listen(app.get("port"), function () {
   console.log("App listening on port " + process.env.PORT);
 });
